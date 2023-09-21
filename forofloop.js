895e756d0726b159f563de0b170a6f14cf805353
Akash kumar (sky) <theakash.education07@gmail.com>
2023-09-21 12:15:56 -0700

    forofloop

---
+++ forofloop.js
@@ -0,0 +1,5 @@
+// Use a 'for...of' loop to iterate over each character in the string "akashMeahuman".
+for (let a of "akashMeahuman") {
+    // Print each character to the console.
+    console.log(a);
+}
