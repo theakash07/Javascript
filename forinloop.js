a2720e89806d12358540faa61ada277b400dee17
Akash kumar (sky) <theakash.education07@gmail.com>
2023-09-21 11:56:58 -0700

    Commented code

--- forinloop.js
+++ forinloop.js
@@ -1,11 +1,14 @@
-let obj={
-    akash:23,
-    sky:45,
-    praneeta:78,
-    metahu:90,
-    homal:77
-}
-for(let a in obj)
-{
-    console.log("The key : "+a+ " and their marks are :"+ obj[a])
+// Define an object 'obj' with key-value pairs representing student names and their marks.
+let obj = {
+    akash: 23,
+    sky: 45,
+    praneeta: 78,
+    metahu: 90,
+    homal: 77
+};
+
+// Use a 'for...in' loop to iterate through the keys (student names) in the 'obj' object.
+for (let a in obj) {
+    // Print the key (student name) and their corresponding marks.
+    console.log("The key: " + a + " and their marks are: " + obj[a]);
 }
