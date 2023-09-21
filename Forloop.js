6a0bcc1a6d9350bc7ab7a128d98cb1aecf831185
Akash kumar (sky) <theakash.education07@gmail.com>
2023-09-21 11:45:42 -0700

    Update with comment

--- Forloop.js
+++ Forloop.js
@@ -1,7 +1,13 @@
-let sum=0
-let n=prompt('Enter value of n')
-for(let i=0;i<n;i++)
-{
-    sum+=(i+1)
+// Initialize the variable 'sum' to store the sum of numbers.
+let sum = 0;
+
+// Prompt the user to enter a value for 'n'.
+let n = prompt('Enter value of n');
+
+// Use a 'for' loop to iterate from 1 to 'n' and calculate the sum.
+for (let i = 0; i < n; i++) {
+    sum += (i + 1); // Add the current number (i+1) to the sum.
 }
-console.log("The sum of"+n+"digits is"+ sum);   
+
+// Display the result using 'console.log'.
+console.log("The sum of " + n + " digits is " + sum);
